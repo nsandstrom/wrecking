898dae2036d539982ecbd8340edb2eadb1cfc0f6
Niklas <mr.n.sandstrom@gmail.com>
2016-06-19 15:14:42 +0200

    outline for owner change added to task (#8)

--- modem_task.h
+++ modem_task.h
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-class Modem_task{
+class Modem_task {
 private:
 	enum Status {New, Pending, Done, Failed};
 	Status status;
@@ -8,11 +8,27 @@ private:
 	enum Task {get_time, send_data, send_status};
 public:
 	bool has_new();
+	bool completed ();
+	void setOwner (enum Owner);
 };
 
+
+//for modem code
 bool Modem_task::has_new (){
 	if( status == New ) {
 		return true;
 	}
 	return false;
 }
+
+
+
+
+//For main code
+bool Modem_task::completed (){
+	false;
+}
+
+void Modem_task::setOwner (enum Owner) {
+	//change Status and Task
+} 
