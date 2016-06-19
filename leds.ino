881f7c0f26ab4e6959c6d48a1d4712e0ac5c3a3d
Niklas <mr.n.sandstrom@gmail.com>
2016-06-19 14:21:58 +0200

    Integrate leds and keypad into master (#6)
    * working on leds stuff
    
    * updateringar på tåg
    
    * 1
    
    * added a select owner function
    
    Added a select owner funtion to wrecker.ino
    
    * Senaste teständringen
    
    bara en dummy äöndring för att testa git

--- leds.ino
+++ leds.ino
@@ -1,17 +1,23 @@
 #define NUM_LEDS 32
 #define MAX_BRIGHT 255
-#define LOW_BRIGHT 16
+#define FLASH_BRIGHT 127
+#define LOW_BRIGHT 32
 #define LEDS_MAX_FADE_BRIGHT 20
 #define LEDS_MIN_FADE_BRIGHT 4
 
-#define SECTIONS 2
+#define SECTIONS 4
 #define SECTION_LENGTH NUM_LEDS/SECTIONS
 
-#define LED_UPDATE_INTERVAL 30
-#define LED_FADE_INTERVAL 60
+#define LED_UPDATE_INTERVAL 50
+#define LED_FADE_INTERVAL 100
+#define LED_FLASH_INTERVAL 100
 
 //this variable keeps track if leds shall be redrawn
 bool redrawLeds = true;
+
+//this array keeps track of if a section is flashing or blacked out
+//0 means that the section is operating normaly
+static byte led_sections_broken[SECTIONS];
 
 //define LED array
 CRGB leds[NUM_LEDS];
@@ -29,11 +35,14 @@ CHSV led_colors[6] = {colorNeutral,
                       colorCyberCom,
                       colorKlustret,
                       colorHjortkloe,
-                      colorBlack};
+                      colorBlack
+                     };
 
 void init_leds() {
+  //make sure all sections start att unbroken  
+  leds_reset_broken_sections();
   FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(leds, NUM_LEDS);
-  FillAll(led_colors[global_owner], LOW_BRIGHT);
+  leds_fill_all(led_colors[global_owner], LOW_BRIGHT);
   FastLED.show();
 }
 
@@ -41,7 +50,7 @@ void leds_update() {
   static unsigned long led_animate_time;
   switch (global_state)
   {
-    case idle:  
+    case idle:
       if ((global_loop_start_time - led_animate_time) > LED_FADE_INTERVAL)
       {
         leds_fade();
@@ -55,7 +64,18 @@ void leds_update() {
         leds_animate_running();
         led_animate_time = global_loop_start_time;
       }
-      
+      break;
+
+    case capturing:
+      if ((global_loop_start_time - led_animate_time) > LED_UPDATE_INTERVAL)
+      {
+        leds_animate_running();
+        FlashLedRow();
+        led_animate_time = global_loop_start_time;
+      }
+      break;
+
+    case waitForCoordinates:
       break;
 
     default:
@@ -69,109 +89,103 @@ void leds_update() {
   }
 }
 
-void leds_fade(){
+void leds_fade() {
   static byte brightness = 0;
   static bool count_up = true;
 
-  FillAll(led_colors[global_owner], brightness);
+  leds_fill_all(led_colors[global_owner], brightness);
 
-  if (count_up){
+  if (count_up) {
     brightness ++;
   } else {
     brightness --;
   }
-  if( brightness == LEDS_MAX_FADE_BRIGHT){
+  if ( brightness == LEDS_MAX_FADE_BRIGHT) {
     count_up = false;
-  }else if ( brightness == LEDS_MIN_FADE_BRIGHT){
+  } else if ( brightness == LEDS_MIN_FADE_BRIGHT) {
     count_up = true;
   }
 }
 
 void leds_animate_running() {
   static int dot = 0;
-  int preDot = dot + 1;
-  int afterDot = dot - 1;
-  int lastDot = dot - 2;
+  int preDot;
+  int afterDot;
+
+  //set pre and after-dot
+  preDot = dot + 1;
+  afterDot = dot - 1;
+  //if dot is at the last position, set predot to the first
+  if (dot >= (SECTION_LENGTH - 1))
+    preDot = 0;
+  //if dot is at the first position, set afterdot to the last
+  else if (dot == 0)
+    afterDot = SECTION_LENGTH - 1;
 
   //reset all colors
-  FillAll(led_colors[global_owner], LOW_BRIGHT);
+  //leds_fill_all(led_colors[global_owner], LOW_BRIGHT);
 
   for (int section = 0; section < SECTIONS; section++) {
     int sectionOffset;
     int sectionDot;
     int sectionPredot;
     int sectionAfterDot;
-    int sectionLastDot;
 
-    //First calculate dot position for the different section
-    //if odd section number, reverse flow
-    if (section & 0x01) {
-      sectionOffset = (section + 1) * SECTION_LENGTH - 1;
+    //only update section if it is not broken
+    if (led_sections_broken[section]==0)
+    {
+      //First calculate dot position for the different section
+      //if odd section number, reverse flow
+      if (section == 2 || section == 3) {
+        sectionOffset = ((section + 1) * SECTION_LENGTH) - 1;
 
-      //preglow
-      sectionPredot = sectionOffset - preDot;
+        //preglow
+        sectionPredot = sectionOffset - preDot;
 
-      //mainGLow
-      sectionDot = sectionOffset - dot;
+        //mainGLow
+        sectionDot = sectionOffset - dot;
 
-      //afterglow
-      sectionAfterDot = sectionOffset - afterDot;
+        //afterglow
+        sectionAfterDot = sectionOffset - afterDot;
+      }
+      //if even section number, let the flow be normal
+      else {
+        sectionOffset = section * SECTION_LENGTH;
 
-      //lastGlow
-      sectionLastDot = sectionOffset - lastDot;
-    }
-    //if even section number, let the flow be normal
-    else {
-      sectionOffset = section * SECTION_LENGTH;
+        //preglow
+        sectionPredot = sectionOffset + preDot;
 
-      //preglow
-      sectionPredot = sectionOffset + preDot;
+        //mainGLow
+        sectionDot = sectionOffset + dot;
 
-      //mainGLow
-      sectionDot = sectionOffset + dot;
+        //afterglow
+        sectionAfterDot = sectionOffset + afterDot;
+      }
 
-      //afterglow
-      sectionAfterDot = sectionOffset + afterDot;
+      //reset the entire section
+      FillSection(led_colors[global_owner], LOW_BRIGHT, section);
 
-      //lastGlow
-      sectionLastDot = sectionOffset + lastDot;
-    }
-
-    //Then update LEDS
-
-    //only draw preglow if within bounds
-    if (preDot < SECTION_LENGTH) {
+      //Then update LEDS
       leds[sectionPredot] = led_colors[global_owner];
       leds[sectionPredot] %= 100;
-    }
 
-    //only draw mainglow if within bounds
-    if (dot < SECTION_LENGTH) {
       leds[sectionDot] = led_colors[global_owner];
-    }
 
-    //only draw afterglow if within bounds
-    if (afterDot >= 0) {
       leds[sectionAfterDot] = led_colors[global_owner];
       leds[sectionAfterDot] %= 100;
     }
-
-    //only draw Lastdot if within bounds
-    if (lastDot >= 0) {
-      leds[sectionLastDot] = led_colors[global_owner];
-      leds[sectionLastDot] %= LOW_BRIGHT;
-    }
   }
 
+  //increment and wrap dot
   dot++;
-  if (dot > SECTION_LENGTH)
+  if (dot >= SECTION_LENGTH)
     dot = 0;
 
   //redraw leds at the end of next loop
   redrawLeds = true;
 }
 
-void FillAll(CHSV color, byte brightness) {
+void leds_fill_all(CHSV color, byte brightness) {
   for (int dot = 0; dot < NUM_LEDS; dot++) {
     leds[dot] = color;
     leds[dot] %= brightness;
@@ -195,24 +209,45 @@ void FillSection (CHSV color, byte brightness, byte section) {
 }
 
 void FlashLedRow() {
+  
 
   for (byte section = 0; section < SECTIONS; section++) {
-    static bool flashing = false;
-
-    //if flashing, dim and stop flash
-    if (flashing) {
-      FillSection(colorNeutral, (64), section);
-      flashing = false;
-    } //If not currently flashing, randomize for flash
+    //if led_sections_broken, indicates a flashing situation, dim and set to 0
+    if (led_sections_broken[section]>100) {
+      FillSection(colorNeutral, 64, section);
+      led_sections_broken[section] = 0;
+    } 
+    //if led_sections_broken indicates an outage, decrement outage counter
+    else if (led_sections_broken[section]>0){
+      led_sections_broken[section]--;
+    }
+    //If not currently active, randomize for flash
     else {
       int randNumber = random(300);
-      if (randNumber > 295) {
-        FillSection(colorNeutral, MAX_BRIGHT, section);
-      }
-      else if (randNumber > 275) {
-        FillSection(colorBlack, 0, section);
+      if (randNumber > (180+(global_capture_countdown*2))) {
+        
+        //four in one triggers should flash, the rest should be blacked out
+        if ((randNumber&0x0F) == 0){
+          led_sections_broken[section] = 101;
+          FillSection(colorNeutral, FLASH_BRIGHT, section);
+        }
+        else
+          led_sections_broken[section] = 3;
+          FillSection(colorBlack, 0, section);
       }
     }
   }
+}
+
+//reset all broken sections
+void leds_reset_broken_sections(){
+  for (int i = 0; i < SECTIONS; ++i)
+  {
+    led_sections_broken[i] = 0;
+  }
+}
+
+void leds_turn_off_all(){
+  leds_fill_all(led_colors[5], 0);
 }
 
