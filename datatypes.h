36dcc0e36acc812204721889974b15a2bdcbb0cd
DunderStorm <joakim@fina.se>
2016-06-19 15:11:56 +0200

    Added functions that updates the owner using modem

--- datatypes.h
+++ datatypes.h
@@ -2,13 +2,14 @@ enum States {
   idle      = 0,
   active    = 1,
   capturing = 2,
-  waitForCoordinates = 3
+  waitForCoordinates = 3,
+  changeOwner = 4
 };
 
 enum Owner {
-  neutral,
-  kaos,
-  cybercom,
-  klustret,
-  hjortkloe
+  neutral  	= 0,
+  kaos		= 1,
+  cybercom	= 2,
+  klustret	= 3,
+  hjortkloe = 4
 };
