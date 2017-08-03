190b9c77da693c21c65c716c4f8f509b23676253
DunderStorm <joakim@fina.se>
2017-08-03 17:16:10 +0200

    blink effects and updated calibration code input
    Some text displayed is now blinking and the calibration code input has
    been changed so that the user now has to click c to submit it (instead
    of autosubmit when hitting 8 digits)

--- teamInfo.h
+++ teamInfo.h
@@ -5,10 +5,10 @@
 #define TEAM4SHORT "     "
 
 //team name must be 12 characters
-#define TEAM1NAME "The Cluster "
-#define TEAM2NAME "Jofa Family "
-#define TEAM3NAME "SnaphaneKlan"
-#define TEAM4NAME "            "
+#define TEAM1NAME "The Cluster  "
+#define TEAM2NAME "Jofa Family  "
+#define TEAM3NAME "SnapphaneKlan"
+#define TEAM4NAME "             "
 
 //team hue
 #define TEAM1HUE 200
