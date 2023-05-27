45c89ac4dea1a2db9755f03ace47e9efdc724967
DunderStorm <joakim654@gmail.com>
2023-05-27 22:05:28 +0200

    bugfix and necececairy doublie init

--- keypad.ino
+++ keypad.ino
@@ -5,7 +5,7 @@ char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
-  {'#', '0', '*', 'D'}
+  {'*', '0', '#', 'D'}
 };
 
 // Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
@@ -14,14 +14,15 @@ byte rowPins[KEYPAD_ROWS] = { KEYPAD_ROW0, KEYPAD_ROW1, KEYPAD_ROW2, KEYPAD_ROW3
 byte colPins[KEYPAD_COLS] = { KEYPAD_COL0, KEYPAD_COL1, KEYPAD_COL2, KEYPAD_COL3 };
 
 //declare the global keypad variable
-Keypad global_kpd = global_kpd = Keypad( makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS );
+Keypad global_kpd = Keypad( makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS );
 
 void init_keypad() {
 	// Create the Keypad
-	//global_kpd = Keypad( makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS );
+	global_kpd = Keypad( makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS );
 }
 
 char getButton() {
+
   char key = global_kpd.getKey();
   return key;
 }
